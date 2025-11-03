55ada4f4e91aefc192960b55ff9d3eb5c0a4deb3
emreaskinsoftware <emreaskinoffical@mail.com>
2025-11-03 10:52:28 +0300

    Feat (v3.0): Add Production-Ready (Gunicorn, Whitenoise, PostgreSQL) config

--- config/settings.py
+++ config/settings.py
@@ -13,28 +13,39 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from django.urls import reverse_lazy
 import environ
+import dj_database_url
 
 # .env dosyasını okumak için 'kıdemli' (senior) kurulum
 env = environ.Env()
 
-# Projenin ana dizinindeki (BASE_DIR) '.env' dosyasını oku
-environ.Env.read_env(env_file=BASE_DIR / '.env')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Projenin ana dizinindeki (BASE_DIR) '.env' dosyasını oku
+environ.Env.read_env(env_file=BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
+# DEBUG=True (Geliştirme) mi, DEBUG=False (Üretim) mi?
+# '.env' dosyasından 'DEBUG' adında bir değişken OKU.
+# Eğer bulamazsan, "profesyonelce" (senior) davran ve 
+# varsayılan (default) olarak 'False' (GÜVENLİ) ayarla.
+DEBUG = env.bool('DEBUG', default=False)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS (İzin Verilen Sunucular)
+# DEBUG = False (Üretim) ise bu alan ZORUNLUDUR.
+# 'localhost' (Geliştirme) ve '.railway.app' (Üretim) 
+# adreslerine "profesyonelce" (senior) izin ver.
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '.railway.app', # <-- Railway'in bize vereceği link
+]
 
 # Application definition
 
@@ -50,10 +61,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -86,10 +99,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': env.db('DATABASE_URL', default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
 }
 
 
@@ -129,6 +139,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
